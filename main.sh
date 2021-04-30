8fc6faa73a8524c2f32246266357a128ceceb2d5
BlackGary <61087466+BlackGaryC@users.noreply.github.com>
2021-04-30 21:07:05 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+source gloableVar.sh
+source fun.sh
+if checkRoot == 0
+then
+        echo -e $noRootRuntimeWaringText
+fi
+#cd /etc/mysql/mariadb.conf.d/
+#sed -i 's/127.0.0.1/0.0.0.0/' 50-server.cnf
+#sed -i 's/#port/port/' 50-server.cnf
+cd $cerentPWD
